c58570009509bd93ee41d8b1e6ef3aa92dab09f2
Lucas Queiroz <lucas.fn4@gmail.com>
2024-12-05 15:37:23 -0300

    adds ntp packet struct

--- src/main.go
+++ src/main.go
@@ -6,6 +6,24 @@ import (
 	"os"
 )
 
+type ntpPacket struct {
+	uli_vn_mode     int8
+	ustratum        int8
+	upoll           int8
+	uprecision      int8
+	urootDelay      int32
+	urootDispersion int32
+	urefId          int32
+	urefTm_s        int32
+	urefTm_f        int32
+	uorigTm_s       int32
+	uorigTm_f       int32
+	urxTm_s         int32
+	urxTm_f         int32
+	utxTm_s         int32
+	txTm_f          uint32
+}
+
 func getCommandLineArguments() ([]string, error) {
 	args := os.Args[1:]
 	correctUsage := "Modo correto de usar:\n./client <ip do servidor NTP>"
