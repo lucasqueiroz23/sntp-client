6cf35d7b7b696ba7e5f42281eae37f360c3ba7ba
Lucas Queiroz <lucas.fn4@gmail.com>
2024-12-06 18:56:44 -0300

    adds function to get current year based on ntp response

--- parser/parser.go
+++ parser/parser.go
@@ -8,10 +8,16 @@ import (
 	"sntp-client/error-handling"
 )
 
+const baseYear uint32 = 1900       // int NTP, the base date is jan 1, 1900
+const daysInAYear float64 = 365.25 // the .25 accounts for years that have 366 days
+const hoursInADay float64 = 24.0
+const minutesInAnHour float64 = 60.0
+const secondsInAMinute float64 = 60.0
+
 func Parse(serverResponse []byte) string {
 	packet := getResponsePacket(serverResponse)
 
-	fmt.Println(packet.TxTm_s)
+	fmt.Println("ano atual: ", getCurrentYear(packet.TxTm_s))
 	return ""
 }
 
@@ -25,4 +31,8 @@ func getResponsePacket(serverResponse []byte) *clientSocket.NtpPacket {
 	}
 
 	return response
+}
+
+func getCurrentYear(timePassedInSeconds uint32) uint32 {
+	return baseYear + uint32(float64(timePassedInSeconds)/(daysInAYear*hoursInADay*minutesInAnHour*secondsInAMinute))
 }
